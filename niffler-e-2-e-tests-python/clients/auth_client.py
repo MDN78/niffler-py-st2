5468052540eb55ffb401cc7f05ae290ca403b99c
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-04-01 15:50:03 +0300

    add oauth

--- niffler-e-2-e-tests-python/clients/auth_client.py
+++ niffler-e-2-e-tests-python/clients/auth_client.py
@@ -2,6 +2,7 @@ import base64
 import hashlib
 import os
 import re
+import pkce
 from urllib.parse import urlparse, parse_qs
 
 from requests import Session
@@ -27,21 +28,33 @@ class AuthSession(Session):
 
 
 class AuthClient:
+    """Авторизует по Oauth2.0"""
+
     def __init__(self, env: Envs):
+        """Генерируем code_verifier и code_challenge. И генерируем basic auth token из секрета сервиса авторизации."""
         self.session = AuthSession()
         self.domain_url = env.auth_url
-        self.code_verifier = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
-        self.code_verifier = re.sub('[^a-zA-Z0-9]+', '', self.code_verifier)
 
-        self.code_challenge = hashlib.sha256(self.code_verifier.encode('utf-8')).digest()
-        self.code_challenge = base64.urlsafe_b64encode(self.code_challenge).decode('utf-8')
-        self.code_challenge = self.code_challenge.replace('=', '')
+        # Этот код мы написали самостоятельно и заменили на целевую схему с использованием библиотеки
+        # self.code_verifier = base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
+        # self.code_verifier = re.sub('[^a-zA-Z0-9]+', '', self.code_verifier)
+        #
+        # self.code_challenge = hashlib.sha256(self.code_verifier.encode('utf-8')).digest()
+        # self.code_challenge = base64.urlsafe_b64encode(self.code_challenge).decode('utf-8')
+        # self.code_challenge = self.code_challenge.replace('=', '')
 
         self._basic_token = base64.b64encode(env.auth_secret.encode('utf-8')).decode('utf-8')
         self.authorization_basic = {"Authorization": f"Basic {self._basic_token}"}
+
+        self.code_verifier, self.code_challenge = pkce.generate_pkce_pair()
         self.token = None
 
     def auth(self, username, password):
+        """Возвращает token oauth для авторизации пользователя с username и password
+        1. Получаем jsessionid и xsrf-token куку в сессию.
+        2. Получаем code из redirec по xsrf-token'у.
+        3. Получаем access_token.
+        """
         session = AuthSession()
 
         session.get(
