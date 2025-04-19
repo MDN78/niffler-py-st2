45c98af8e1c5496dcb5062586ef8445f8a49a595
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-04-19 18:20:58 +0300

    update project

--- niffler-e-2-e-tests-python/utils/allure_helper.py
+++ niffler-e-2-e-tests-python/utils/allure_helper.py
@@ -1,5 +1,4 @@
 import json
-import logging
 from json import JSONDecodeError
 
 import allure
@@ -8,48 +7,50 @@ from allure_commons.types import AttachmentType
 from requests import Response
 
 
-# from jinja2 import TemplateError, Environment, PackageLoader, select_autoescape
-# from requests.structures import CaseInsensitiveDict
-
-
 def allure_attach_request(function):
     """Декоратор логироваания запроса, хедеров запроса, хедеров ответа в allure шаг и аллюр аттачмент и в консоль."""
 
     def wrapper(*args, **kwargs):
         method, url = args[1], args[2]
+        from jinja2 import Environment, PackageLoader, select_autoescape
+        env = Environment(
+            loader=PackageLoader("resources"),
+            autoescape=select_autoescape()
+        )
+
+        template = env.get_template('colored_request.ftl')
+
         with allure.step(f"{method} {url}"):
             response: Response = function(*args, **kwargs)
 
             curl = curlify.to_curl(response.request)
-            logging.debug(curl)
-            logging.debug(response.text)
+
+            prepare_render = {
+                "request": response.request,
+                "curl": curl,
+            }
+            render = template.render(prepare_render)
 
             allure.attach(
-                # body=curlify.to_curl(curl).encode("utf-8"),
-                body=curl.encode('utf8'),
-                name=f"Request {response.status_code}",
-                attachment_type=AttachmentType.TEXT,
-                extension=".txt"
+                body=render,
+                name=f"Request",
+                attachment_type=AttachmentType.HTML,
+                extension=".html"
             )
             try:
                 allure.attach(
                     body=json.dumps(response.json(), indent=4).encode("utf8"),
                     name=f"Response json {response.status_code}",
                     attachment_type=AttachmentType.JSON,
-                    extension=".json"
+                    extension=".html"
                 )
-            except JSONDecodeError:
+            except (JSONDecodeError, TypeError):
                 allure.attach(
                     body=response.text.encode("utf8"),
                     name=f"Response text {response.status_code}",
                     attachment_type=AttachmentType.TEXT,
                     extension=".txt")
-            allure.attach(
-                body=json.dumps(dict(response.headers, indent=4)).encode("utf8"),
-                name=f"Response headers {response.status_code}",
-                attachment_type=AttachmentType.JSON,
-                extension=".json"
-            )
+
         return response
 
     return wrapper
